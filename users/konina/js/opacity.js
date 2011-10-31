75bb521b45f0eb6ff503d278c38bd0ff99d79ae5
Yelena Konina <konina@mail.ru>
2011-10-31 15:07:28 +0300

    Signed-off-by: Yelena Konina <konina@mail.ru>

--- users/konina/js/opacity.js
+++
@@ -1,9 +0,0 @@
-window.attachEvent('onload', makeOpacity);
-
-function makeOpacity(){
-	var figcap = document.getElementsByTagName('FIGCAPTION');
-	for(var i=0; i<figcap.length; i++){
-		figcap[i].insertAdjacentHTML('beforeBegin','<div></div>');
-		figcap[i].previousSibling.setAttribute('id','figfon');
-	}
-};
