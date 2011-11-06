4112ccf94d5006b3883a5f52210ba9b8e1b94a1f
User <User@Users-MacBook-Pro.local>
2011-11-06 02:33:29 +0200

    plov

---
+++ users/3em/baring/index.decl.js
@@ -0,0 +1,83 @@
+exports.blocks = [
+    {
+        "name": "i-global"
+    },
+    {
+        "name": "b-header"
+    },
+    {
+        "name": "b-wrapper"
+    },
+    {
+        "name": "b-logo"
+    },
+    {
+        "name": "b-menu",
+        "elems": [
+			{
+				"name": "list"
+			},
+            {
+				"name": "item"
+			},
+            {
+				"name": "icon"
+			}
+
+        ]
+    },
+    {
+        "name": "b-menu-drop",
+        "elems": [
+			{
+				"name": "list"
+			},
+            {
+				"name": "item"
+			}
+        ]
+    },
+    {
+        "name": "b-search",
+        "elems": [
+			{
+				"name": "icon"
+			}
+        ]
+    },
+    {
+        "name": "b-content"
+    },
+    {
+        "name": "b-article"
+    },
+    {
+        "name": "b-link"
+    },
+    {
+        "name": "b-site-bar"
+    },
+    {
+        "name": "b-sub-menu",
+        "elems": [
+			{
+				"name": "active"
+			}
+        ]
+    },
+    {
+        "name": "b-partners"
+    },
+    {
+        "name": "b-footer"
+    },
+    {
+        "name": "b-language",
+        "elems": [
+			{
+				"name": "arrow"
+			}
+        ]
+    }
+
+]
